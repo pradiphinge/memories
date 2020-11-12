6822a45319b1e10e57ce223cfb7ddbdafb0b919f
Pradipkumar Hinge <hingepradeepster@gmail.com>
2020-11-12 11:05:28 +0530

    posts route setup done

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 
 import connectDB from './db.js';
+import postRoutes from './routes/posts.js'
 
 const app = express();
+app.use('/posts', postRoutes);
 
 dotenv.config();
 app.use(cors());
 
+
 const PORT = process.env.PORT 
 connectDB()
 
+app.listen(PORT,console.log(`server running on ${process.env.NODE_ENVIRONMENT} mode on port ${PORT}`.yellow.bold));
